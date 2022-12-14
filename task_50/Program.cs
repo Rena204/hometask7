4bd05209c0c617f8b32fc05780d791d56c3b912e
rena <rena2004.rr@gmail.com>
2022-12-14 13:13:10 +0400

    hometask

---
+++ task_50/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+Console.WriteLine("введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+ArrayRandom(numbers);
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
+}
+
+
+void ArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-10, 11);
+                Console.Write($"{array[i, j]} \t");
+            }   
+            Console.WriteLine();
+        }
+}
---
+++ task_52/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.WriteLine("введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+ArrayRandom(numbers);
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double result = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        result += numbers[j, i];
+    }
+    result = result / n;
+    Console.Write(result + "; ");
+}
+
+void ArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-10, 11);
+                Console.Write($"{array[i, j]} \t");
+            }   
+            Console.WriteLine();
+        }
+}
